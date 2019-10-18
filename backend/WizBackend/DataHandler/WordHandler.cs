7e3bb019df853e30ccc6f3aae16d9aed2b56ee49
Danielh21 <danilhollmann@hotmail.com>
2019-10-18 14:43:33 +0200

    Added API and datahandler

---
+++ backend/WizBackend/DataHandler/WordHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace WizBackend.DataHandler
+{
+    public class WordHandler
+    {
+
+        static private string connectionString = @"Data Source = C:\Users\Daniel Hollmann\Documents\Development\Source\wizfile\database\Dictionary.db; 
+                                    Version=3; FailIfMissing=True; Foreign Keys = True;";
+
+        public static void LookUpWord(string parts)
+        {
+            SQLiteConnection e = new SQLiteConnection(connectionString, true);
+            e.Open();
+            Console.Write("Bla");
+        }
+    }
+}
