66000c89da72b643f10680c9323e8df8b0a9554f
Danielh21 <danielhollmann@hotmail.com>
2019-10-18 14:43:33 +0200

    Added API and datahandler

---
+++ backend/WizBackend/Controllers/WordsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WizBackend.DataHandler;
+
+namespace WizBackend.Controllers
+{
+    [Route("api/words")]
+    [ApiController]
+    public class WordsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            WordHandler.LookUpWord("few");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
