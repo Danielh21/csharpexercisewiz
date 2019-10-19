db1e0a4d8a97a9fa2fe8efc078da3c9397865066
Danielh21 <danilhollmann@hotmail.com>
2019-10-19 21:32:02 +0200

    Implemented the backend webservice task

--- backend/WizBackend/Controllers/WordsController.cs
+++ backend/WizBackend/Controllers/WordsController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Cors;
 using WizBackend.DataHandler;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace WizBackend.Controllers
 {
@@ -14,6 +17,10 @@ namespace WizBackend.Controllers
     [Produces("application/json")]
     public class WordsController : ControllerBase
     {
+
+        private static readonly HttpClient client = new HttpClient();
+
+
         // GET api/values
         [HttpGet("db/{part}")]
         public JsonResult DatabaseResult(string part)
@@ -26,9 +33,19 @@ namespace WizBackend.Controllers
         }
 
         [HttpGet("webservice/{part}")]
-        public JsonResult Get(string part)
+        public async Task<JsonResult> WebServiceResult(string part)
         {
+            // File is ignored with git
+            var token = System.IO.File.ReadAllText("Token.txt");
 
+            client.DefaultRequestHeaders.Authorization
+                         = new AuthenticationHeaderValue("Bearer", token);
+            var response = await client.GetAsync("https://services.lingapps.dk/misc/getPredictions?locale=da-DK&text=" + part);
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            return new JsonResult(new
+            {
+
+            });
         }
 
 
