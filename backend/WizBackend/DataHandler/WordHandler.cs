4e7a373e19c66fa6a4ab0508dbe9bf4c299a86f1
Danielh21 <danilhollmann@hotmail.com>
2019-10-19 16:37:04 +0200

    Made the backend/database implementation

--- backend/WizBackend/DataHandler/WordHandler.cs
+++ backend/WizBackend/DataHandler/WordHandler.cs
@@ -14,9 +14,34 @@ namespace WizBackend.DataHandler
 
         public static void LookUpWord(string parts)
         {
+            List<String> MatchingWords = new List<String>();
             SQLiteConnection e = new SQLiteConnection(connectionString, true);
-            e.Open();
-            Console.Write("Bla");
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM Words w WHERE w.value like 'e%'";
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                    {
+                        using (SQLiteDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                MatchingWords.Add(reader["value"].ToString());
+                            }
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            catch (SQLiteException exp)
+            {
+                throw exp;
+            }
+
+            Console.WriteLine(MatchingWords.Count);
+
         }
     }
 }
